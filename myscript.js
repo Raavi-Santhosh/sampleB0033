de1049ad16353395b18c53f50011918f23deb9ce
Santhosh Raavi <tra@Santhoshs-MacBook-Pro.local>
2023-09-27 09:17:16 +0530

    second commit

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(json => console.log(json));
 
-
+console.log("hello all");
 
 
 function users(){
